a90c3938dd17771220d136df9d3119e2020745f4
torman <torman_cheng@yahoo.com>
2013-09-15 11:30:58 -0400

    Changed doctype declaration and add content-type

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
 <html>
 <head>
 	<title>Welcome to P1</title>
+		<meta http-equiv="content-type" 
+		content="text/html;charset=utf-8" />
 </head>
 <body>
 <h1>Welcome to P1</h1>
