f7a4ba18439bc197f22ca651f2e007cefe640bc4
torman <torman_cheng@yahoo.com>
2013-09-15 11:55:57 -0400

    changed to <br />

--- index.php
+++ index.php
@@ -9,21 +9,21 @@
 </head>
 <body>
 <h1>Welcome to P1</h1>
-<p>
+<br />
 
 <!-- php script starts here -->
 <?php
 	echo "This is Torman Cheng.";
-	echo "<p>";
+	echo "<br />";
 	echo "I am a software QA engineer working on an open scource project named \"OpenScholar\". ";
 	echo "If you want to know more OpenScholar, go to ";
 	echo "<a href=\"openscholar.harvard.edu\">";
 	echo "here";
 	echo "</a>";
-	echo "<p>";
+	echo "<br />";
 	echo "This is the first time I create a real website. ";
 	echo "Very exciting!!!! ";
-	echo "<p>";
+	echo "<br />";
 	echo "Now, I am using notepad++ as code editor for this class, but also use vi in linux system. "; 
 	echo "Miss the old friend vi. Suddenly I need it to modify .bashrc. ";
 	echo "I recall that was the reason I learned vi long time ago."; 
