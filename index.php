148f96012d9b8ad20fab454d8765279f6f4caa03
torman <torman_cheng@yahoo.com>
2013-09-15 15:14:05 -0400

    change <br /> to avoid validation issue

--- index.php
+++ index.php
@@ -16,16 +16,16 @@
 <?php
 	echo "<p>";
 	echo "This is Torman Cheng.";
-	echo "<br>";
+	echo "<br />";
 	echo "I am a software QA engineer working on an open scource project named \"OpenScholar\". ";
 	echo "If you want to know more OpenScholar, go to ";
 	echo "<a href=\"openscholar.harvard.edu\">";
 	echo "here";
 	echo "</a>";
-	echo "<br>";
+	echo "<br />";
 	echo "This is the first time I create a real website. ";
 	echo "Very exciting!!!! ";
-	echo "<br>";
+	echo "<br />";
 	echo "Now, I am using notepad++ as code editor for this class, but also use vi in linux system. "; 
 	echo "Miss the old friend vi. Suddenly I need it to modify .bashrc. ";
 	echo "I recall that was the reason I learned vi long time ago.";
