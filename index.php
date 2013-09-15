d24e64864d15e9a61a47383357496e320630d8d2
torman <torman_cheng@yahoo.com>
2013-09-15 15:09:02 -0400

    try <p><h1>Welcome to P1</h1></p> to avoid the validation error

--- index.php
+++ index.php
@@ -8,25 +8,28 @@
 		content="text/html;charset=utf-8" />
 </head>
 <body>
-<h1>Welcome to P1</h1>
-<br></br>
+
+<p><h1>Welcome to P1</h1></p>
+
 
 <!-- php script starts here -->
 <?php
+	echo "<p>";
 	echo "This is Torman Cheng.";
-	echo "<br />";
+	echo "<br>";
 	echo "I am a software QA engineer working on an open scource project named \"OpenScholar\". ";
 	echo "If you want to know more OpenScholar, go to ";
 	echo "<a href=\"openscholar.harvard.edu\">";
 	echo "here";
 	echo "</a>";
-	echo "<br />";
+	echo "<br>";
 	echo "This is the first time I create a real website. ";
 	echo "Very exciting!!!! ";
-	echo "<br />";
+	echo "<br>";
 	echo "Now, I am using notepad++ as code editor for this class, but also use vi in linux system. "; 
 	echo "Miss the old friend vi. Suddenly I need it to modify .bashrc. ";
-	echo "I recall that was the reason I learned vi long time ago."; 
+	echo "I recall that was the reason I learned vi long time ago.";
+	echo "</p>";
 ?>	
 </body>
 </html>
