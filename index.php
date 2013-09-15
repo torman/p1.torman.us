59f080f101ae3872e72c29cd79d0dccaed3256fa
torman <torman_cheng@yahoo.com>
2013-09-15 10:28:56 -0400

    Added my short bio

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-Welcome to P1
+<html>
+<head>
+</head>
+<body>
+<h1>Welcome to P1</h1>
+<br>
+
+<!-- php script starts here -->
+<?php
+	echo "This is Torman Cheng.";
+	echo "<br>";
+	echo "<br>";
+	echo "I am a software QA engineer working on an open scource project named \"OpenScholar\". ";
+	echo "If you want to know more OpenScholar, go to ";
+	echo "<a href=\"openscholar.harvard.edu\">";
+	echo "here";
+	echo "</a>";
+	echo "<br>";
+	echo "This is the first time I create a real website. ";
+	echo "Very exciting!!!! ";
+	echo "<br>";
+	echo "<br>";
+	echo "Now, I am using notepad++ as code editor for this class, but also use vi in linux system. "; 
+	echo "Miss the old friend <strong>vi</strong>. Suddenly I need it to modify .bashrc. ";
+	echo "I recall that was the reason I learned vi long time ago."; 
+?>	
+</body>
+</html>
